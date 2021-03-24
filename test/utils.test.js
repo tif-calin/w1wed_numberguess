1d8bbd07e3ba6b0e215d7e87215b19d00a62808c
tif-calin <culitif@tuta.io>
2021-03-24 15:52:45 -0700

    add heart display and lose condition

--- test/utils.test.js
+++ test/utils.test.js
@@ -9,5 +9,5 @@ test('time to test a function', (expect) => {
     expect.equal(compareNumbers(corrNum, '14'), 'correct');
     expect.equal(compareNumbers(corrNum, 15.5), 'too high');
     expect.equal(compareNumbers(corrNum, '13.5'), 'too low');
-    expect.equal(compareNumbers(corrNum, -12), 'too low');
+    expect.equal(compareNumbers(corrNum, -17), 'too low');
 });
--- utils.js
+++ utils.js
@@ -1,3 +1,4 @@
+const healthbar = document.getElementById('lives');
 
 export const compareNumbers = (n, g) => {
     const num = Number(n);
@@ -7,3 +8,17 @@ export const compareNumbers = (n, g) => {
     else if (guess > num) return 'too high';
     else return 'too low';
 };
+
+export const displayHearts = n => {
+    const num = Number(n);
+
+    healthbar.innerHTML = '';
+
+    for (let i = 0; i < num; i++) {
+        const heart = document.createElement('img');
+        heart.src = './assets/health.png';
+        heart.classList.add('heart');
+
+        healthbar.appendChild(heart);
+    }
+}
