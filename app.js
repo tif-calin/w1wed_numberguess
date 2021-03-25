3967420762937e426bb150216bae67219c54e9b5
tif-calin <culitif@tuta.io>
2021-03-24 17:00:20 -0700

    shows stats

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const btnGuess = document.getElementById('button-guess');
 const divGame = document.getElementById('game');
 const inpGuess = document.getElementById('input-guess');
 const guessResult = document.getElementById('guess-result');
+const gameStats = document.getElementById('game-stats');
 
 // initialize state
 const gameState = {
@@ -15,12 +16,28 @@ const gameState = {
 const pastScores = [];
 
 // set event listeners to update state and DOM
+const updateStats = () => {
+    const highScore = Math.min(...pastScores);
+    // const averageScore = Math.sum(...pastScores) / pastScores.length;
+    let winRate = 0;
+    for (const num of pastScores) {
+        if (num !== 5) winRate++;
+    }
+    winRate /= pastScores.length;
+    winRate = (winRate * 100).toFixed(2) + '%';
+
+    if (pastScores.length > 1) gameStats.textContent = `High score: ${highScore} (win rate: ${winRate})`;
+};
+
 const didLose = () => {
     if (!gameState['livesLeft']) {
         btnGuess.disabled = true;
         guessResult.textContent = 'You lost :(';
         btnPlay.classList.remove('hidden');
-    }
+        pastScores.push(5);
+
+        return true;
+    } else return false;
 };
 
 btnPlay.addEventListener('click', () => {
@@ -48,10 +65,12 @@ btnGuess.addEventListener('click', () => {
     if (result === 'correct') {
         btnPlay.classList.remove('hidden');
         btnGuess.disabled = true;
+        pastScores.push(5 - gameState['livesLeft']);
+        updateStats();
     } else {
         gameState['livesLeft']--;
     }
 
     displayHearts(gameState['livesLeft']);
-    didLose();
+    if (didLose()) updateStats();
 });
